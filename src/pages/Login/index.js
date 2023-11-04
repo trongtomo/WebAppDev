7e10f12deaf6ee879c7332d9a5f1bb0a1c00e62c
trongtomo <binguyenquoctrong@gmail.com>
2023-11-05 00:04:55 +0700

    fix loginPage.module.css to LoginPage.module.css

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import LoginForm from "../../../components/LoginForm/loginForm"; // Make sure the import path is correct
-import styles from "./loginPage.module.css"; // Import styles from loginPage.module.css
+import styles from "./LoginPage.module.css"; // Import styles from loginPage.module.css
 
 const LoginPage = () => {
   return (
